f881d2cefda11101091c3b3eee9b02fbf0751da3
Arthur Tena <arthur.tena@etu.umontpellier.fr>
2023-11-15 17:47:23 +0100

    modifs

--- extraction.py
+++ extraction.py
@@ -9,20 +9,11 @@ from IPython.display import display, HTML
 #On affiche le jour de la semaine 
 maintenant = datetime.datetime.now()
 
-# Calculez la date de fin 
-date_fin = maintenant + datetime.timedelta(days=4)
-
-# Formattez les dates au format AAAA-MM-JJ.
-date_debut= maintenant.strftime('%Y-%m-%d')
-date_fin= date_fin.strftime('%Y-%m-%d')
-
-
 # Obtenir le jour de la semaine actuel (en anglais)
 jour_semaine = maintenant.strftime('%A')  # Format du jour de la semaine (par exemple : "lundi", "mardi", etc.)
 
 #On met l'URL de la clef API pour la retrouver avec requests
 url= 'https://api.open-meteo.com/v1/meteofrance?latitude=43.62&longitude=3.86&hourly=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=Europe%2FBerlin'
-meteo_url = f'{url}&start_date={date_debut}&end_date={date_fin}'
 reponse=requests.get(url)
 
 api_key = 'API météo Montpellier'
@@ -38,7 +29,7 @@ params = {
 	"daily": ["temperature_2m_max", "temperature_2m_min", "sunrise", "sunset"],
 }
 
-response = requests.get(meteo_url, headers=headers, params=params)
+response = requests.get(url, headers=headers, params=params)
 if response.status_code == 200:
     # Obtenir le jour de la semaine actuel (en anglais)
  jour_semaine = maintenant.strftime('%A')  # Format du jour de la semaine (par exemple : "lundi", "mardi", etc.)
@@ -108,6 +99,7 @@ if response.status_code == 200:
  temp_min=data[0]['daily']['temperature_2m_min']
  sunrise=data[0]['daily']['sunrise']
  sunset=data[0]['daily']['sunset']
+ #On regarde a quelle heure de la joournée on se situt 
  def heure():
     for i in range(24):
        time[0][i]=time[0][i+1]
@@ -115,20 +107,32 @@ if response.status_code == 200:
        if (a==True):
           return (i)
  Température=[(temp[0][heure()],temp_max[0]),(temp_max[1],temp_min[1]),(temp_max[2],temp_min[2]),(temp_max[3],temp_min[3])]
- Humidité=[(humidity[0][heure()],np.mean(humidity[0])),(np.mean(humidity[1]),np.max(humidity[1])),(np.mean(humidity[2]),np.max(humidity[2])),(np.mean(humidity[3]),np.max(humidity[3]))]
+ Humidité=[(humidity[0][heure()],np.mean(humidity[0])),(np.max(humidity[1]),np.mean(humidity[1])),(np.max(humidity[2]),np.mean(humidity[2])),(np.max(humidity[3]),np.mean(humidity[3]))]
  Vent=[(wind[0][heure()],np.mean(wind[1])),(np.max(wind[1]),np.min(wind[1])),(np.max(wind[2]),np.min(wind[2])),(np.max(wind[3]),np.min(wind[3]))]
  Pluie=[(precipitation[0][heure()],np.mean(precipitation[0])),(np.max(precipitation[1]),np.mean(precipitation[1])),(np.max(precipitation[2]),np.mean(precipitation[2])),(np.max(precipitation[3]),np.mean(precipitation[3]))]
  Soleil=[(sunrise[0],sunset[0]),(sunrise[1],sunset[1]),(sunrise[2],sunset[2]),(sunrise[3],sunset[3])]
- tab=[[Température[i] for i in range(4)],
-      [Humidité[i] for i in range(4)],
-      [Vent[i] for i in range(4)],
-      [Pluie[i] for i in range(4)],
-      [Soleil[i] for i in range(4)]
-      ]
- Tab=pd.DataFrame(tab,index=['Temperature (en °C)','Humidite (en %)','Vitesse du vent (en km/h)','Precipitation (en mm)','Sunrise/Sunset'],
+ tab=[[f"actuellement {Température[0][0]}°C, au max {Température[0][1]}°C",
+ f"au min {Température[1][0]}°C, au max {Température[1][1]}°C",
+ f"au min {Température[2][0]}°C, au max {Température[2][1]}°C",
+ f"au min {Température[3][0]}°C, au max {Température[3][1]}°C"],
+ [f"actuellement {Humidité[0][0]}%, en moyenne {Humidité[0][1]}%",
+ f"au max {Humidité[1][0]}%, en moyenne {Humidité[1][1]}%",
+ f"au max {Humidité[2][0]}%, en moyenne {Humidité[2][1]}%",
+ f"au max {Humidité[3][0]}%, en moyenne {Humidité[3][1]}%"],
+ [f"actuellement {Vent[0][0]}km/h, en moyenne {Vent[0][1]}km/h",
+ f"au max {Vent[1][0]}km/h, au min {Vent[1][1]}km/h",
+ f"au max {Vent[2][0]}km/h, au min {Vent[2][1]}km/h",
+ f"au max {Vent[3][0]}km/h, au min {Vent[3][1]}km/h"],
+ [f"actuellement {Pluie[0][0]}mm, en moyenne {Pluie[0][1]}mm",
+ f"au max {Pluie[1][0]}mm, en moyenne {Pluie[1][1]}mm",
+ f"au max {Pluie[2][0]}mm, en moyenne {Pluie[2][1]}mm",
+ f"au max {Pluie[3][0]}mm, en moyenne {Pluie[3][1]}mm"],
+ [Soleil[i] for i in range(4)]]
+ Tab=pd.DataFrame(tab,index=['Temperature','Humidite','Vitesse du vent$','Precipitation','Sunrise/Sunset'],
     columns=[jours()[0],jours()[1],jours()[2], jours()[3]])
  html_Tab = Tab.to_html('meteo.html')
- display(HTML(html_Tab))
+ print(html_Tab)
+ #display(HTML(html_Tab))
 
 
 
